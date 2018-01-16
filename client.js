b3201e72fe4f1e5d57ce78952e025ec3f373b610
sophiapeckner <31379599+sophiapeckner@users.noreply.github.com>
2018-01-15 19:32:51 -0800

    before show to Mr. Chow

--- client.js
+++ client.js
@@ -20,19 +20,22 @@ var setup = function () {
     house = loadImage("https://cdn.glitch.com/5daadb6b-664c-4228-9e6b-6b4cb1cdc269%2Fhouse.jpg?1515908309315");
     pup = loadImage("https://cdn.glitch.com/5daadb6b-664c-4228-9e6b-6b4cb1cdc269%2FcutePup.jpg?1515908300442");
     pup2 = loadImage("https://cdn.glitch.com/5daadb6b-664c-4228-9e6b-6b4cb1cdc269%2Fpup2.jpg?1515909836003");
+    lightBackground = loadImage("https://cdn.glitch.com/5daadb6b-664c-4228-9e6b-6b4cb1cdc269%2FcoolBackground.jpg?1516067824714");
+    courtHouse = loadImage("https://cdn.glitch.com/5daadb6b-664c-4228-9e6b-6b4cb1cdc269%2Fcourthouse.jpg?1516070746343");
     page = "start";
     click = false;
     mouseIn = false;
     record = false;
     time = 0;
     t0 = 0;
-    id = round(random(200));
+    id = int(random(200));
     currentTime = hour()+":"+minute();
     car1 = 8;
     car2 = 6;
     car3 = 4;
     car4 = 2;
     defaultTextColor = "black";
+    thinkingmanX = 0;
 };
 
 var drawCar = function (x, y, color) {
@@ -47,7 +50,12 @@ var drawCar = function (x, y, color) {
 var recording = function (key, value) {
     //var database = firebase.database().ref();
     //database.child(id+"/"+key).set(value);
-    firebase.database().ref("test/" + id + "/" + key).set(value);
+    if (id % 2 === 0) {
+      level = "hard";
+    }else{
+      level = "easy";
+    }
+    firebase.database().ref("test/" + level + "/" + id + "/" + key).set(value);
 };
 
 var hardPic = function () {
@@ -58,7 +66,7 @@ var hardPic = function () {
     drawCar(car3, 360, fill(0, 255, 40));
     drawCar(car2, 460, fill(255, 0, 10));
     drawCar(car1, 560, fill(255, 200, 0));
-    car4 += 8;
+    car4 += 7;
     car3 += 7;
     car2 += 6;
     car1 += 6;
@@ -67,14 +75,13 @@ var hardPic = function () {
         image(ogperson, 280, 220, 70, 70);
     } else if (car1 > 290) {
         //dead stick man
-        car4 += 2;
+        car4 += 5;
         car3 = 300;
         car2 = 300;
         car1 = 300;
         fill(0);
-        text("OUCH! WAWA", 320, 145, 150, 70);
         image(dead, 300, 160, 100, 100);
-        //image(ogperson, 250, 220, 70, 70);
+        text("OUCH! WAWA", 290, 140, 150, 70);
         image(ogperson, ogpersonX, 220, 70, 70);
         if (ogpersonX <= 250) {
             ogpersonX += 8;
@@ -85,7 +92,7 @@ var hardPic = function () {
     }
     //distractions
     fill(135, 54, 0);
-    text("FITNESS CENTER!", 5, 130, 200, 100);
+    text("FITNESS CENTER!", 5, 85, 200, 100);
     image(yeildsign, 420, 160, 70, 70);
     image(liftweight, 20, 20, 90, 90);
     image(ballworkout, 100, 20, 90, 90);
@@ -102,22 +109,20 @@ var hardPic = function () {
 };
 
 var simplePic = function () {
-        reset(); 
+    reset(); 
     drawCar(car4, 260, fill(255, 220, 220));
-    drawCar(car3, 360, fill(0, 255, 40));
-    car4 += 3;
-    car3 += 4;
-    //distractions
-    image(yeildsign, 420, 160, 70, 70);
-    image(house, 210, -15, 200, 140);
+    //drawCar(car3, 360, fill(0, 255, 40));
+    car4 += 7;
+    //car3 += 4;
+    //image(yeildsign, 420, 160, 70, 70);
     if (car4 < 290) {
         image(ogperson, 280, 220, 70, 70);
     } else if (car4 > 290) {
-        car4 += 2;
-        car3 = 300;
+        car4 += 5;
+        //car3 = 300;
         fill(0);
-        text("OUCH! WAWA", 320, 145, 150, 70);
         image(dead, 300, 160, 100, 100);
+        text("OUCH! WAWA", 290, 140, 150, 70);
     }
 };
 
@@ -172,24 +177,32 @@ var draw = function () {
 
     if (page === "start") {
         reset();
-        recording("time", currentTime);
-        button(125, 210, 180, 60, 40, "start", defaultTextColor, "pic");
+        image(lightBackground);
+        button(200, 250, 180, 60, 40, "start", defaultTextColor, "pic");
+        textSize(17);
+        strokeWeight(1);
+        fill(0);
+        text("To reduce contamination of data, please do NOT refresh the page", 80, 400, 400, 100);
+        text("Thank u for participating in my study.\n This program will take < 2 min to complete", 80, 50, 400, 100);
     }
     if (page === "pic") {
-        //if(id % 2 === 0){//even
-        hardPic();
-        //}else{
-        //simplePic();
-        //}
+        if(id % 2 === 0){//even
+          hardPic();
+        }else{
+          simplePic();
+        }
 
         timer(4, "Q1");
     }
 
     if (page === "Q1") {
         reset();
-        text("What was the color of the car that hit the person?", 15, 35, 400, 80);
-        button(125, 110, 150, 60, 50, "pink", "pink", "Q2", true, "Q1First");
-        button(125, 210, 150, 60, 50, "green", "green", "Q2", true, "Q1First");
+        text("What was the color of the car that hit the person?", 0, 15, 400, 80);
+        button(125, 70, 150, 60, 50, "green", "green", "Q2", true, "Q1First");
+        button(125, 140, 150, 60, 50, "pink", "pink", "Q2", true, "Q1First");
+        button(125, 280, 150, 60, 50, "red", "red", "Q2", true, "Q1First");
+        button(125, 210, 150, 60, 50, "blue", "blue", "Q2", true, "Q1First");
+        button(125, 350, 150, 60, 29, "none of\n the above", "black", "Q2", true, "Q1First");
         click = false;
     }
     if (page === "Q2") {
@@ -201,26 +214,31 @@ var draw = function () {
     }
     if (page === "transition") {
         reset();
-        textSize(20);
+        textSize(40);
         noStroke();
-        text("Please recall what you just saw for a moment...", 100, 20, 400, 100);
-        image(thinkingman, 20, 120, 200, 200);
+        text("Hmm ... Let me think about this", 0, 200, 600, 100);
+        image(thinkingman, thinkingmanX, 400, 200, 200);
+        image(courtHouse, 400, 400, 200, 200);
+        thinkingmanX += 2;
         timer(8, "Q1Again");
 
     }
     if (page === "Q1Again") {
-        reset();
-        text("what was the car's color?", 0, 35, 400, 80);
-        button(125, 110, 150, 60, 50, "pink", "pink","Q2Again", true, "Q1Again");
-        button(125, 210, 150, 60, 50, "blue", "blue", "Q2Again", true, "Q1Again");
-        click = false;
+      reset();
+      text("What was the color of the car that hit the person?", 0, 15, 400, 80);
+      button(125, 70, 150, 60, 50, "green", "green", "Q2Again", true, "Q1Again");
+      button(125, 140, 150, 60, 50, "pink", "pink", "Q2Again", true, "Q1Again");
+      button(125, 280, 150, 60, 50, "red", "red", "Q2Again", true, "Q1Again");
+      button(125, 210, 150, 60, 50, "blue", "blue", "Q2Again", true, "Q1Again");
+      button(125, 350, 150, 60, 29, "none of\n the above", "black", "Q2Again", true, "Q1Again");
+      click = false;
     }
 
     if (page === "Q2Again") {
         reset();
         text("Was there a stop sign?", 10, 35, 400, 80);
-        button(125, 110, 150, 60, 50, "yes", defaultTextColor, "gender", true, "Q2Again");
-        button(125, 210, 150, 60, 50, "no", defaultTextColor,"gender", true, "Q2Again");
+        button(125, 110, 150, 60, 50, "Yes", defaultTextColor, "gender", true, "Q2Again");
+        button(125, 210, 150, 60, 50, "No", defaultTextColor,"gender", true, "Q2Again");
         click = false;
     }
 
@@ -239,9 +257,11 @@ var draw = function () {
         button(20, 240, 300, 60, 50, "18 and over", defaultTextColor,"bye", true, "age");
     }
     if (page === "bye") {
-        background(178, 214, 206);
+        recording("time", currentTime);
+        image(lightBackground);
         fill(0, 0, 0);
         text("Thank you =)", 10, 20, 400, 80);
     }
     click = false;
 };
+
