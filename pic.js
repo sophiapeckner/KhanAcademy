736bb6f4fa9b970fdb24574041b16e8af72eaeb9
Sophia Peckner <sophiapeckner@gmail.com>
2018-01-17 23:07:55 -0800

    800x700 frame

--- pic.js
+++ pic.js
@@ -1,7 +1,7 @@
 var reset = function () {
     background(255, 255, 255);
     frameX = 800;
-    frameY = 800;
+    frameY = 700;
     createCanvas(frameX, frameY);
     fill("black");
     noStroke();
@@ -46,16 +46,16 @@ var setup = function () {
     car11 = 1000;
     car12 = 900;
     soccerballX = 200;
-    soccerballY = 440;
+    soccerballY = 340;
     soccerballBackX=540;
-    soccerballBackY=373;
+    soccerballBackY=273;
     toprunManX = 350;
-    toprunManY = 310;
+    toprunManY = 210;
     topkickManX = 540;
     topkickrunX = 310;
     btmkickManX = 180;
     crossingPersonX = 800;
-    crossingPersonY = 400;
+    crossingPersonY = 300;
     wait1=200;
     wait2=400;
 };
@@ -70,54 +70,52 @@ var drawCar = function (x, y, color) {
 };
 
 var up = function(){
-    image(treeBack, 0, 0, 800, 500);
+    image(treeBack, 0, 0, 800, 400);
     image(soccerball, soccerballX, soccerballY, 40, 30);
-    image(btmkickMan, btmkickManX, 400, 70, 70);
+    image(btmkickMan, btmkickManX, 300, 70, 70);
     soccerballX += 3;
     soccerballY -= 0.5;
     if (soccerballX >= 448){
-        image(toprunMan, toprunManX, 300, 80, 80)
+        image(toprunMan, toprunManX, 200, 80, 80)
         toprunManX += 4.7;
     }else{
-        image(callforballMan, 400, 300, 90, 90);
+        image(callforballMan, 400, 200, 90, 90);
     }
 
     if(soccerballX >= 543){
         soccerballX = 540;
-        soccerballY = 373;
+        soccerballY = 273;
         //topkickManX = 540;
-        topkickManY = 350;
+        topkickManY = 250;
         toprunManX = 900;
-        image(btmwavingMan, 180, 400, 90, 90);
+        image(btmwavingMan, 180, 300, 90, 90);
         btmkickManX = 900;
         soccerballX = 900;
         textSize(18);
         strokeWeight(2);
-        //text("over here!", 170, 360, 100, 50);
-        image(topkickMan, topkickManX, 310, 80, 80);
+        text("over here!", 170, 260, 100, 50);
+        image(topkickMan, topkickManX, 210, 80, 80);
         image(soccerball, soccerballBackX, soccerballBackY, 40, 30);
         soccerballBackX -= 3;
     }
-    if (soccerballBackX > 100) {
-        text("over here!", 170, 360, 100, 50);
-    } else if (soccerballBackX < 100){
-        image(topkickMan, topkickManX, topkickrunX, 80, 80);
-        text ("i'll get it!", topkickManX + 5, 280, 80, 50);
+    if (soccerballBackX < 100){
+        //image(topkickMan, topkickManX, topkickrunX, 80, 80);
+        text ("i'll get it!", topkickManX + 5, 180, 80, 50);
         topkickManX -= 3;
     }
         
 }
 var bottom = function(){
-    image(road, 0, 500, 800, 300)
+    image(road, 0, 400, 800, 300)
     fill("white");
     for (i = 0; i < 300; i = i + 20) {
-        rect(400, 520 + i, 70, 10);
+        rect(400, 420 + i, 70, 10);
     }
     image(crossingPerson, crossingPersonX, crossingPersonY, 70, 70);
-    drawCar(car1, 520, fill("pink"));
-    drawCar(car2, 600, fill("blue"));
-    drawCar(car3, 680, fill("red"));
-    drawCar(car4, 760, fill("yellow"));
+    drawCar(car1, 420, fill("pink"));
+    drawCar(car2, 500, fill("blue"));
+    drawCar(car3, 580, fill("red"));
+    drawCar(car4, 660, fill("yellow"));
     car1 += 4;
     car2 += 3;
     car3 -= 4;
@@ -126,10 +124,10 @@ var bottom = function(){
     if (wait1>0){
         wait1--;
     } else if (wait1===0){
-        drawCar(car5, 520, fill("grey"));
-        drawCar(car6, 600, fill("orange"));
-        drawCar(car7, 680, fill("red"));
-        drawCar(car8, 760, fill("green"));
+        drawCar(car5, 420, fill("grey"));
+        drawCar(car6, 500, fill("orange"));
+        drawCar(car7, 580, fill("red"));
+        drawCar(car8, 660, fill("green"));
         car5 += 4;
         car6 += 3;
         car7 += 3;
@@ -139,10 +137,10 @@ var bottom = function(){
     if (wait2>0){
         wait2--;
     } else if (wait2===0){
-        drawCar(car9, 520, fill("red"));
-        drawCar(car10, 600, fill("blue"));
-        drawCar(car11, 680, fill("yellow"));
-        drawCar(car12, 760, fill("green"));
+        drawCar(car9, 420, fill("red"));
+        drawCar(car10, 500, fill("blue"));
+        drawCar(car11, 580, fill("yellow"));
+        drawCar(car12, 660, fill("green"));
         if (car10 < 350) {
             car9 += 3;
             car10 += 3;
@@ -155,9 +153,9 @@ var bottom = function(){
         } else if (car10 === 350) {
             car11 -= 5;
             crossingPersonY += 2;
-            if (crossingPersonY > 600) {
+            if (crossingPersonY > 500) {
                 crossingPersonY = 900;
-                image(crossingPersonAngry, crossingPersonX, 600, 70, 70)
+                image(crossingPersonAngry, crossingPersonX, 500, 70, 70)
             }
         }
     }
